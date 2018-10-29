1b9e3ea0e61b6872ef51810534faaf264814f81c
Mike Vanis <hello@mikevanis.com>
2018-10-29 12:16:52 +0000

    flipped buttons if flip_video is set to true, so that they match up with VisionCam Tall

--- LinedrawingTimelapse.py
+++ LinedrawingTimelapse.py
@@ -58,10 +58,18 @@ class LinedrawingTimelapse(Thread):
         # Setup buttons
         if gpio_exists:
             logging.info("GPIO exists. Setting up pins...")
-            self.btn1 = Button(17)
-            self.btn2 = Button(27)
-            self.btn3 = Button(23)
-            self.btn4 = Button(22)
+
+            if self.config["flip_video"] is 1:
+                self.btn1 = Button(27)
+                self.btn2 = Button(17)
+                self.btn3 = Button(22)
+                self.btn3 = Button(23)
+            else:
+                self.btn1 = Button(17)
+                self.btn2 = Button(27)
+                self.btn3 = Button(23)
+                self.btn4 = Button(22)
+
             self.btn_main = self.btn1
 
         self.anim_lenscap = ["assets/lensecap_white_1.png", "assets/lensecap_white_2.png"]
